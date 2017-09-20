1b641a9898841de2772d6f7a7bd79b92dea22d54
angelo.santos <angelo.santos@castgroup.com.br>
2017-09-20 10:53:59 -0300

    Core module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,60 +1,24 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {InputTextModule} from 'primeng/components/inputtext/inputtext';
-import {ButtonModule} from 'primeng/components/button/button';
-import {DataTableModule} from 'primeng/components/datatable/datatable';
-import {TooltipModule} from 'primeng/components/tooltip/tooltip';
-import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { CampoColoridoDirective } from './campo-colorido.directive';
-import { FormsComponent } from './forms/forms.component';
-import {FormsModule} from '@angular/forms';
-import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
-import {InputTextareaModule} from 'primeng/components/inputtextarea/inputtextarea';
-import {CalendarModule} from 'primeng/components/calendar/calendar';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {SelectButtonModule} from 'primeng/components/selectbutton/selectbutton';
-import {DropdownModule} from 'primeng/components/dropdown/dropdown';
-import {CurrencyMaskModule} from 'ng2-currency-mask';
-import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
-import {InputMaskModule} from 'primeng/components/inputmask/inputmask';
-import { MessageComponent } from './message/message.component';
-import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.component';
-import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
-
-
+import {PessoasModule} from './pessoas/pessoas.module';
+import {LancamentosModule} from './lancamentos/lancamentos.module';
+import {CoreModule} from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LancamentosPesquisaComponent,
-    NavbarComponent,
-    PessoasPesquisaComponent,
-    CampoColoridoDirective,
-    FormsComponent,
-    LancamentoCadastroComponent,
-    PessoaCadastroComponent,
-    MessageComponent,
-    LancamentosGridComponent,
-    PessoasGridComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    InputTextModule,
-    ButtonModule,
-    DataTableModule,
-    TooltipModule,
-    FormsModule,
-    InputTextareaModule,
-    CalendarModule,
     BrowserAnimationsModule,
-    SelectButtonModule,
-    DropdownModule,
-    InputMaskModule,
-    CurrencyMaskModule
+
+    LancamentosModule,
+    PessoasModule,
+
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
