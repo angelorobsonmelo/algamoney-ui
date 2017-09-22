a71098609964d06351878c3667925a8a8febd5d2
angelo.santos <angelo.santos@castgroup.com.br>
2017-09-22 15:34:07 -0300

    Adicionando o nav bar com routerLink

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,6 @@
 import {LOCALE_ID, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {NavbarComponent} from '../navbar/navbar.component';
+import {NavbarComponent} from './navbar/navbar.component';
 import {ErrorHandlerService} from './error-handler.service';
 import {ToastyModule} from 'ng2-toasty';
 import {ConfirmationService, ConfirmDialogModule} from 'primeng/primeng';
