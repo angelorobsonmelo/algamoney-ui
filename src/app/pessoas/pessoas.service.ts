216f24a161604d24efc4cd5d111fa875cdb492ba
Ângelo Robson <angelorobsonmelo@gmail.com>
2017-09-21 22:09:20 -0300

    Paginação servidor pessoa implementada.

---
+++ src/app/pessoas/pessoas.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers, URLSearchParams } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+export class PessoaFiltro {
+  nome: string;
+  pagina = 0;
+  itensDaPagina = 5;
+}
+
+@Injectable()
+export class PessoasService {
+
+  pessoasUrl = 'http://localhost:8080/pessoas';
+  constructor(private http: Http) { }
+
+  pesquisar(filtro: PessoaFiltro): Promise<any>{
+    const params = new URLSearchParams();
+    const headers = new Headers();
+
+    headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+    params.set('page', filtro.pagina.toString());
+    params.set('size', filtro.itensDaPagina.toString());
+
+    if(filtro.nome){
+      params.set('nome', filtro.nome);
+    }
+
+    return this.http.get(`${this.pessoasUrl}`, {headers, search: params})
+      .toPromise().then(response => {
+        const responseJson = response.json();
+        const pessoas = responseJson.content;
+
+        const resultado = {
+          pessoas,
+          total: responseJson.totalElements
+        };
+
+        return resultado;
+      });
+
+  }
+
+  listarTodas(): Promise<any> {
+    const headers = new Headers();
+    headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+   return this.http.get(`${this.pessoasUrl}`, {headers})
+      .toPromise()
+      .then(response => response.json().content);
+  }
+
+
+}
